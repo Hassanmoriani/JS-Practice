7148862ae0f950bab55a18080074b1f0cc9bc528
Hassanmoriani <64978920+Hassanmoriani@users.noreply.github.com>
2024-03-11 01:54:08 +0500

    conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,34 @@
+let score = true
+
+
+
+console.log(typeof(score))
+
+
+let valueInNumber = Number(score)
+console.log(typeof(valueInNumber))
+console.log(valueInNumber)
+
+
+// "33" = > 33
+// "33abc" => NaN
+// true => 1; false => 0
+
+
+let isLoggedIn = ""
+
+let booleanLoggedIn = Boolean(isLoggedIn)
+console.log(typeof(booleanLoggedIn))
+console.log(booleanLoggedIn)
+
+
+// 1 = > true; 0 = > fasle
+// "" => fasle
+// "Hassan" => true
+
+
+let somenumber  = 33
+
+let stringNumber  = String(somenumber)
+console.log(somenumber)
+console.log(typeof stringNumber)
