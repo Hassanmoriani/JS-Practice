f597b347e6fb47239e2b7b6a1e09bcf6c54652cc
Hassanmoriani <64978920+Hassanmoriani@users.noreply.github.com>
2024-03-12 23:28:38 +0500

    Datatype Summary & comparision

---
+++ 01_basics/datatype-summary.js
@@ -0,0 +1,40 @@
+// Primtive 
+
+// 7 types : string. Number, booleam. Null, undefined, Symbol, BigInt
+
+const score = 100
+
+const scoreValue = 100.12
+
+const isLoggedIn = false
+
+const outSideTemp = null
+
+let userEmail;
+
+const id = Symbol("145")
+const anotherId = Symbol("145") 
+
+console.log(id === anotherId)
+
+const bigNumber = 748747478484848481548
+
+// Refernce Or (Non Primtive)
+
+// Array , Objects , Functions 
+
+const heros = ["Superman","Batman", "spiderman"]
+
+let myObject ={
+    name : "hassan",
+    age : 25,
+}
+
+console.log(myObject.name)
+
+
+const myFunction = function(){
+    console.log("Hey function");
+}
+
+console.log(typeof anotherId);
