7d0b46504a1b8aecd8b35722eacf47f693d5b02d
Hassanmoriani <64978920+Hassanmoriani@users.noreply.github.com>
2024-03-14 23:59:27 +0500

    numbers & Maths functions in js done

---
+++ 01_basics/07_nums_and_maths.js
@@ -0,0 +1,39 @@
+const  score  = 400
+// console.log(score)
+
+const balance = new Number(100)
+// console.log(balance);
+
+
+// console.log(balance.toString().length)
+// console.log(balance.toFixed(2))
+
+
+const othernumber = 1123.8966
+// console.log(othernumber.toPrecision(3));
+
+
+const hundreds = 1000000
+// console.log(hundreds.toLocaleString('en-PK'))
+// console.log(hundreds.toLocaleString('en-IN'))
+
+
+// +++++++++++++ Maths ++++++++++++++++++++ //
+
+// console.log(Math.PI.toFixed(2));
+// console.log(Math.abs(-4))
+// console.log(Math.round(4.66))
+// console.log(Math.ceil(4.2))
+// console.log(Math.floor(4.8))
+// console.log(Math.sqrt(64))
+
+// console.log(Math.random());
+console.log((Math.random()*10) + 1);
+// console.log((Math.random()*10) + 1);
+
+
+const min = 10
+const max = 20
+
+// console.log((Math.random() * (max - min +1 )) + min )
+console.log(Math.floor(Math.random() * (max - min +1 )) + min )
