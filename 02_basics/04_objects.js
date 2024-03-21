c9f2b9a256eb71f11256f923756c502432129e7c
Hassanmoriani <64978920+Hassanmoriani@users.noreply.github.com>
2024-03-22 02:44:57 +0500

    object de structure done

---
+++ 02_basics/04_objects.js
@@ -0,0 +1,33 @@
+const course = {
+    coursename: "js in hindi",
+    price: "999",
+    courseInstructor: "Hitesh"
+}
+
+// course.courseInstructor
+
+// In react 
+// const navbar = ({company}) =>{
+
+// }
+// navbar(company = "HM")
+
+// Obhect de structure
+const {
+    courseInstructor: instructor
+} = course
+
+console.log(instructor);
+
+// {
+//     'name' :"Hassan",
+//     "coursename" :"Js in Hindi",
+//     "price" : "free"
+// }
+
+[
+    {},
+    {},
+    {}, 
+]
+
